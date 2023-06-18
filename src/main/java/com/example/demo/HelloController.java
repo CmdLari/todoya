f3cba70f6467b9c1859ac23745976c7ce5f5800c
Larissa Pychlau <studio-lp@gmx.de>
2023-06-18 17:18:13 +0200

    Some stuff

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private List<String> tasks = new ArrayList<String>();
+
+
+
 }
--- src/main/java/com/example/demo/TasksController.java
+++ src/main/java/com/example/demo/TasksController.java
@@ -1,2 +1,21 @@
-package com.example.demo;public class tasksController {
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.List;
+
+class tasksController {
+
+    private TextField taskEnter = new TextField();
+    private ListView taskList = new ListView<>();
+
+
+    public void addToTaskList(List<String> tasks) throws IOException {
+
+        tasks.add("");
+    }
+
 }
